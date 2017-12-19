534badaf2fed44f903d4124d9c6c8f8b70d9abfd
davej33 <davejusten03@yahoo.com>
2017-12-19 14:00:11 -0500

    update color scheme

---
+++ app/src/main/java/com/android/example/oca_808/TestReviewActivity.java
@@ -0,0 +1,13 @@
+package com.android.example.oca_808;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class TestReviewActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_test_review);
+    }
+}
